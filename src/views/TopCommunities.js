175e1a0feedeb88afaef34d7fc9dd823dcfa167b
Fahim <fahimalemroz@gmail.com>
2021-09-06 11:31:35 +0600

    Designed the top communities page

--- src/views/TopCommunities.js
+++ src/views/TopCommunities.js
@@ -3,9 +3,9 @@ import { useSubreddits } from '../hooks';
 
 function TopCommunities() {
 	const subreddits = useSubreddits();
-	useEffect(() => {
-		console.dir(subreddits);
-	}, [subreddits]);
+	// useEffect(() => {
+	// 	console.dir(subreddits);
+	// }, [subreddits]);
 
 	const relevantDataExtracted =
 		subreddits &&
@@ -13,12 +13,12 @@ function TopCommunities() {
 			postId: sub.id,
 			subredditUrl: sub.url,
 			name: sub.display_name_prefixed,
-			iconImg: sub.icon_img,
+			iconImg: sub.icon_img || sub.banner_img,
 			subscribers: sub.subscribers,
 		}));
 
 	return (
-		<div className="text-green-400 font-nunito font-bold mt-24">
+		<div className="text-green-400 font-nunito font-bold mt-10">
 			<h1 className="container bg-gray-500 text-white font-ubuntu rounded-md shadow p-3 text-left pl-5 mb-10">
 				Top Communities
 			</h1>
@@ -28,25 +28,23 @@ function TopCommunities() {
 					relevantDataExtracted.map((sub) => (
 						<li
 							key={sub.postId}
-							className="container mb-5 rounded-md bg-gray-600 shadow"
+							className="container mb-5 rounded-md bg-gray-600 shadow flex items-center py-2 gap-5 pl-5 cursor-pointer"
 						>
-							<a
-								href={`https://www.reddit.com${sub.subredditUrl}`}
-								target="_blank"
-							>
-								<img
-									src={sub.iconImg}
-									className="w-5 h-5 rounded-full border border-2 border-blue-300"
-								/>
-								<div className="subreddit-data">
-									<h2 className="font-nunito text-gray-50 ">
-										{sub.name}
-									</h2>
-									<p className="text-gray-300">
-										{sub.subscribers} members
-									</p>
-								</div>
-							</a>
+							<img
+								src={sub.iconImg}
+								className={`${
+									!sub.iconImg && 'invisible'
+								}  w-6 h-6 rounded-full border border-2 border-blue-400`}
+							/>
+
+							<div className="subreddit-data text-left ">
+								<h2 className="font-nunito text-gray-50 font-extrabold">
+									{sub.name}
+								</h2>
+								<p className="text-gray-300 text-sm">
+									{sub.subscribers} members
+								</p>
+							</div>
 						</li>
 					))}
 			</ul>
