559d976c8bd81b8eb30831d15754a9a23bb66663
Fahim <fahimalemroz@gmail.com>
2021-09-08 07:21:36 +0600

    Abstracted the searchbar away from NavBar as a separate component

---
+++ src/components/SearchBar.js
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+import { SearchIcon } from '../icons';
+
+function SearchBar() {
+	const [searchTerm, setSearchTerm] = useState('');
+	const [searchResults, setSubredditResults] = useState([]);
+
+	// this is the function for fetching subreddits needed from the searchbar input
+	const searchSubreddits = async (searchTerm) => {
+		const apiResponse = await fetch(
+			`https://www.reddit.com/search.json?q=${searchTerm}&type=sr`
+		);
+		const jsonData = await apiResponse.json();
+
+		const subreddits = jsonData.data.children.map(
+			(subreddit) => subreddit.data
+		);
+
+		setSubredditResults((prevData) => (prevData = subreddits));
+	};
+
+	// this fires on every keystroke on the searchbar input, but calls the api only when searchTerm is truthy and on enter press
+	const handleEnterPress = (e) =>
+		searchTerm ? searchSubreddits(searchTerm) : '';
+
+	return (
+		<div className="search-bar flex rounded overflow-hidden">
+			<div className="icon-wrapper bg-red-400 px-2 py-1">
+				<SearchIcon className="text-white" />
+			</div>
+			<input
+				type="text"
+				placeholder="Search subreddits"
+				className="font-nunito text-center bg-gray-600 flex-1 outline-none text-white font-bold"
+				value={searchTerm}
+				onChange={({ keyCode, target: { value: newSearchTerm } }) =>
+					setSearchTerm((prevTerm) => (prevTerm = newSearchTerm))
+				}
+				onKeyUp={(e) => e.keyCode === 13 && handleEnterPress()}
+			/>
+		</div>
+	);
+}
+
+export default SearchBar;
