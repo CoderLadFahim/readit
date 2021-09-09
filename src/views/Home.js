52a6d01d3ade726f95755d6349cb9ba9e75fa10c
Fahim <fahimalemroz@gmail.com>
2021-09-09 17:48:08 +0600

    Made a list of properties needed from post objects returned from usePosts

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import { clearAll } from '../features/subreddits/subredditSlice';
 import { useDispatch } from 'react-redux';
+import { usePosts, useComments, useSubreddits } from '../hooks';
 
 function Home() {
 	// the following variable and side effect, clears the subreddits when user visits the home route
@@ -8,6 +9,16 @@ function Home() {
 	useEffect(() => {
 		subredditSliceActionDispatcher(clearAll());
 	}, []);
+	/*
+	 returns an array of postObjs
+	 properties needed from the post objects: 
+	 		title, over_18 (hide the post if true), author, permalink(need this to fetch comments), subreddit_name_prefixed, ups, downs,
+	*/
+	const posts = usePosts('marriage');
+
+	useEffect(() => {
+		posts && console.dir(posts[1]);
+	}, [posts]);
 
 	return (
 		<section>
