12aa727adbc5e8f3e1ca1b325a2866f001c532ae
Fahim <fahimalemroz@gmail.com>
2021-09-09 18:16:17 +0600

    Added TopCommunitiesBtn that routes user to /subreddits

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,7 +1,9 @@
 import { useEffect } from 'react';
 import { clearAll } from '../features/subreddits/subredditSlice';
 import { useDispatch } from 'react-redux';
+import { ListIcon } from '../icons';
 import { usePosts, useComments, useSubreddits } from '../hooks';
+import { Link } from 'react-router-dom';
 
 function Home() {
 	// the following variable and side effect, clears the subreddits when user visits the home route
@@ -22,9 +24,17 @@ function Home() {
 
 	return (
 		<section>
-			<h1 className="w-1/2 my-0 mx-auto text-center text-white text-2xl font-bold mt-24">
-				Home Route (/)
-			</h1>
+			<div className="container border relative">
+				<h1 className="w-1/2 my-0 mx-auto text-center text-white text-2xl font-bold mt-24">
+					Home Route (/)
+				</h1>
+				<Link
+					className="top-communities-btn absolute transform w-9 h-9 bg-gray-500 rounded-full grid place-items-center right-0 bottom-0 transition hover:scale-110"
+					to="/subreddits"
+				>
+					<ListIcon className="text-gray-50 transform scale-125" />
+				</Link>
+			</div>
 		</section>
 	);
 }
