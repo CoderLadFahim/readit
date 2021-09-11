628ab49fe619dc8e02119741e63f0a490e385911
hanzala <hasanbinomar.0.1.2.3@gmail.com>
2021-09-11 14:00:27 +0600

    created another h1 tag and studied usePosts

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import { clearAll } from '../features/subreddits/subredditSlice';
 import { useDispatch } from 'react-redux';
+import { usePosts, useSubreddits } from '../hooks';
 
 function Home() {
 	// the following variable and side effect, clears the subreddits when user visits the home route
@@ -9,11 +10,20 @@ function Home() {
 		subredditSliceActionDispatcher(clearAll());
 	}, []);
 
+	const posts = usePosts('all');
+	console.log(posts);
+	const subreddits = useSubreddits('starterpacks')
+	console.log(subreddits);
+	//confused  with how to implement usePosts and useSubreddits. it doesnt have anycontent it to show.
 	return (
 		<section>
 			<h1 className="w-1/2 my-0 mx-auto text-center text-white text-2xl font-bold mt-24">
 				Home Route (/)
 			</h1>
+			<h1 className="w-1/2 my-0 mx-auto text-center text-white text-2xl font-bold mt-24">
+				{posts[2].subreddit}
+			</h1>
+			
 		</section>
 	);
 }
