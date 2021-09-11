a5bf3ccc6047c31f3b95101c3432ef128935db4d
Fahim <fahimalemroz@gmail.com>
2021-09-11 07:35:59 +0600

    Added sidebtns functionality, starting TopCommunities bugfix

--- src/components/SideButtons.js
+++ src/components/SideButtons.js
@@ -3,18 +3,17 @@ import { ListIcon } from '../icons';
 import { useQuery } from '../hooks';
 
 function SideButtons() {
-	const history = useHistory();
+	const { location } = useHistory();
 	const query = useQuery();
 	const subredditQuery = query.get('subreddit');
 
 	return (
 		<div className="side-btns absolute right-0 bottom-0 border flex flex-col justify-between">
-			<button
-				className="transform w-9 h-9 bg-gray-200 rounded-full grid place-items-center transition hover:scale-110 shadow sm:hidden"
-				me
-			>
-				<span className="font-bold text-gray-600">r/?</span>
-			</button>
+			{location === '/subreddit' && (
+				<button className="transform w-9 h-9 bg-gray-200 rounded-full grid place-items-center transition hover:scale-110 shadow sm:hidden">
+					<span className="font-bold text-gray-600">r/?</span>
+				</button>
+			)}
 			{subredditQuery !== 'all' && (
 				<Link
 					className="transform w-9 h-9 bg-blue-400 rounded-full grid place-items-center transition hover:scale-110 shadow"
