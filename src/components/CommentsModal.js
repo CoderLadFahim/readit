e5abd0720aeccf4d563dcc93a67f978a8fa49553
Fahim <fahimalemroz@gmail.com>
2021-09-12 22:41:40 +0600

    Addef further adjustments to CommentsModal

--- src/components/CommentsModal.js
+++ src/components/CommentsModal.js
@@ -4,7 +4,7 @@ import { useComments } from '../hooks';
 
 function CommentsModal({ permalink }) {
 	const placeholderPermalink =
-		'/r/nextfuckinglevel/comments/pmj58z/miami_fans_save_cat_from_falling_of_upper_deck/';
+		'/r/nottheonion/comments/pn0pht/emma_raducanu_whose_stunning_victory_at_the_us/';
 
 	const postComments = useComments(
 		permalink ? permalink : placeholderPermalink
@@ -19,42 +19,44 @@ function CommentsModal({ permalink }) {
 
 	return (
 		<section className="modal-container border border-2 border-red-400 text-white">
-			<div className="backdrop bg-gray-800 opacity-95 absolute top-0 bottom-0 right-0 left-0 z-40">
-				<CrossIcon className="absolute top-3 right-3 text-blue-400" />
-			</div>{' '}
+			<div className="backdrop bg-gray-800 opacity-95 absolute top-0 bottom-0 right-0 left-0 z-40"></div>{' '}
 			{postComments && (
 				<div
-					className="comments-modal container h-5/6 bg-gray-600
+					className="comments-modal w-11/12 h-5/6 bg-gray-600
 					z-50 absolute left-1/2 top-1/2 transform -translate-x-1/2
-					-translate-y-1/2 rounded-xl "
+					-translate-y-1/2 rounded-xl"
 				>
 					{' '}
-					<div
-						className="header px-5
-						py-5
-						flex"
-					>
-						<h1 className="font-bold">
-							Comments on u/{postComments.parentPostAuthorName}'s post
+					<div className="header w-11/12 my-0 mx-auto py-5 mb-2 flex items-center justify-between">
+						<h1 className="font-bold text-sm">
+							Comments on{' '}
+							<span className="text-red-400">
+								u/{postComments.parentPostAuthorName}'s
+							</span>{' '}
+							post
 						</h1>
+						<CrossIcon className="text-blue-400" />
 					</div>
 					<ul
 						id="comments"
 						className="w-11/12 my-0 mx-auto h-5/6 overflow-scroll rounded-lg"
 					>
-						{postComments.comments.map((comment, i) => (
-							<li
-								className="comment text-white mb-3 bg-gray-500 rounded-lg px-3 py-2"
-								key={i}
-							>
-								<span className="block commenter-name text-gray-700 font-bold mb-2">
-									u/{comment.commentAuthor}
-								</span>
-								<p className="comment-text font-nunito">
-									{comment.commentText}
-								</p>
-							</li>
-						))}
+						{postComments.comments.map(
+							(comment, i) =>
+								comment.commentText && (
+									<li
+										className="comment text-white mb-2  bg-gray-500 rounded-lg px-3 py-2"
+										key={i}
+									>
+										<span className="block commenter-name text-gray-300  mb-2 text-xs">
+											u/{comment.commentAuthor}
+										</span>
+										<p className="comment-text font-nunito font-bold">
+											{comment.commentText}
+										</p>
+									</li>
+								)
+						)}
 					</ul>
 				</div>
 			)}
