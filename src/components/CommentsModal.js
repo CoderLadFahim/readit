743b5db8d951e10f827c8edeeeeed0ff663d0dd4
Fahim <fahimalemroz@gmail.com>
2021-09-12 11:23:05 +0600

    Showing the comments received from permalink

--- src/components/CommentsModal.js
+++ src/components/CommentsModal.js
@@ -6,30 +6,37 @@ function CommentsModal({ permalink }) {
 	const placeholderPermalink =
 		'/r/nextfuckinglevel/comments/pmj58z/miami_fans_save_cat_from_falling_of_upper_deck/';
 
-	const TEMP_COMMENTS = useComments(placeholderPermalink);
+	const postComments = useComments(
+		permalink ? permalink : placeholderPermalink
+	);
 
 	useEffect(() => {
-		console.dir(TEMP_COMMENTS);
-	}, [TEMP_COMMENTS]);
+		console.dir(postComments);
+	}, [postComments]);
 
 	return (
-		<section className="border border-2 border-red-400">
+		<section className="modal-container border border-2 border-red-400 absolute top-0 bottom-0 left-0 right-0 bg-gray-800 text-white font-nunito">
 			{' '}
-			<div className="header">
-				<h1>Comments on u/knight_monke's post</h1>
-				<CrossIcon />
-			</div>
-			<ul>
-				<li className="comment">
-					<span className="commenter-name">u/knight_monke</span>
-					<p className="comment-text">
-						Adipisicing eveniet nulla quidem sunt deleniti. Tempora modi
-						esse quasi explicabo nobis! Ipsam natus nobis eveniet
-						repellendus architecto ipsam dicta?j
-					</p>
-				</li>
-			</ul>
-			<div className="backdrop bg-gray-800 opacity-95 absolute top-0 bottom-0 right-0 left-0 z-10"></div>
+			{postComments && (
+				<div className="comments-modal container border border-blue-300">
+					<div className="header">
+						<h1>
+							Comments on u/{postComments.parentPostAuthorName}'s post
+						</h1>
+						<CrossIcon />
+					</div>
+					<ul>
+						{postComments.comments.map((comment) => (
+							<li className="comment text-green-400 mb-3">
+								<span className="commenter-name text-blue-300">
+									u/{comment.commentAuthor}
+								</span>
+								<p className="comment-text">{comment.commentText}</p>
+							</li>
+						))}
+					</ul>
+				</div>
+			)}
 		</section>
 	);
 }
