5cefb9fa9fe9c37d1584f020124ccad9bd62660f
hanzala <hasanbinomar.0.1.2.3@gmail.com>
2021-09-30 08:38:13 +0600

    pulled again

--- src/views/Home.js
+++ src/views/Home.js
@@ -2,13 +2,10 @@ import { useEffect } from 'react';
 import { clearAll } from '../features/subreddits/subredditSlice';
 import { useDispatch } from 'react-redux';
 import { usePosts, useComments, useSubreddits, useQuery } from '../hooks';
-import { Link } from 'react-router-dom';
-<<<<<<< HEAD
+import { Link, Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+import CommentsModal from '../components/CommentsModal';
 import MainPage from '../components/MainPage';
 import LeaderBoard from '../components/LeaderBoard';
-=======
-import CommentsModal from '../components/CommentsModal';
->>>>>>> 178df35bdd768fb507c456efda0bc111624cc9d3
 
 function Home() {
 	// getting the subreddit to look for from query params, (coming initially from CatchAll and also from subreddit results on SubredditDisplay)
@@ -28,17 +25,10 @@ function Home() {
 	*/
 	const posts = usePosts(subredditQuery);
 
-<<<<<<< HEAD
-	useEffect(() => {
-		console.log(subredditQuery);
-		posts && console.log(posts);
-	}, [posts]);
-=======
 	// uncomment the following useEffect blocc to study the data
 	// useEffect(() => {
 	// 	posts && console.dir(posts[1]);
 	// }, [posts]);
->>>>>>> 178df35bdd768fb507c456efda0bc111624cc9d3
 
 	return (
 		<section>
@@ -50,16 +40,19 @@ function Home() {
 					</span>
 				</h1>
 			</div>
-<<<<<<< HEAD
-			<div className='front-page'>
-				{<MainPage posts={posts}></MainPage>}
-				<LeaderBoard></LeaderBoard>
-				
-			</div>
-=======
 			{/* <CommentsModal /> */}
->>>>>>> 178df35bdd768fb507c456efda0bc111624cc9d3
+			<div>
+				<Router>
+					<Switch>
+						<Route path='/'>
+							<MainPage></MainPage>
+							<LeaderBoard></LeaderBoard>
+						</Route>
+					</Switch>
+				</Router>
+			</div>
 		</section>
+		
 	);
 }
 
