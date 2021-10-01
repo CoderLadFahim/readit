6d30349be425e8dd1324c35ba9295e86de774dea
Fahim <fahimalemroz@gmail.com>
2021-10-01 23:30:54 +0600

    Videos working

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -5,6 +5,7 @@ import { useComments } from '../hooks';
 import { ArrowDownIcon, ArrowUpIcon, CommentIcon } from '../icons';
 import CustomSubredditIcon from './CustomSubredditIcon';
 import CommentsModal from './CommentsModal';
+import Post from './Post';
 
 const MainPage = (props) => {
 	let comments = useComments(props.posts[0].permalink);
@@ -22,54 +23,7 @@ const MainPage = (props) => {
 		<main className="post">
 			{props.posts &&
 				props.posts.map((post, i) => {
-					return (
-						<div className="post-detail container bg-gray-700" key={i}>
-							{/* <CommentsModal /> */}
-							<h4 className="flex">
-								<CustomSubredditIcon subName={post.author} />
-								<span>{post.subreddit_name_prefixed}</span>
-								<span>posted by/{post.author}</span>
-							</h4>
-							<div>
-								<h2 className="title">{post.title}</h2>
-							</div>
-							{!post.media && <img src={post.url} />}
-							{props.posts.is_video && (
-								<video width="100%" controls>
-									<source
-										src={post.secure_media.reddit_video.fallback_url}
-										type="video/mp4"
-									></source>
-								</video>
-							)}
-
-							<div className="post-btn">
-								<div>
-									<button className="btn upvote">
-										<ArrowUpIcon /> Upvotes<span>{post.ups}</span>
-									</button>
-									<button className="btn downvote">
-										<ArrowDownIcon /> DownVotes
-										<span>{post.downs}</span>
-									</button>
-
-									<button
-										onClick={() => {
-											// setPermalinkForComments(post.permalink)
-											console.log(post.permalink);
-										}}
-										className="btn comment right"
-									>
-										<CommentIcon /> Comments
-										<span>
-											{/* returns comments for a single post (don't worry about it man, I'll handle everything)*/}
-											{comments && comments.comments.length}
-										</span>
-									</button>
-								</div>
-							</div>
-						</div>
-					);
+					return <Post post={post} key={i} />;
 				})}
 		</main>
 	);
---
+++ src/components/Post.js
@@ -0,0 +1,58 @@
+import { useComments } from '../hooks';
+import { ArrowDownIcon, ArrowUpIcon, CommentIcon } from '../icons';
+import CustomSubredditIcon from './CustomSubredditIcon';
+
+function Post({ post }) {
+	let comments = useComments(post.permalink);
+
+	return (
+		<div className="post-detail container bg-gray-700" key={post.permalink}>
+			{/* <CommentsModal /> */}
+			<h4 className="flex">
+				<CustomSubredditIcon subName={post.author} />
+				<span>{post.subreddit_name_prefixed}</span>
+				<span>posted by/{post.author}</span>
+			</h4>
+			<div>
+				<h2 className="title">{post.title}</h2>
+			</div>
+			{!post.media && <img src={post.url} />}
+			{post.is_video && (
+				<video width="100%" controls>
+					<source
+						src={post.secure_media.reddit_video.fallback_url}
+						type="video/mp4"
+					></source>
+				</video>
+			)}
+
+			<div className="post-btn">
+				<div>
+					<button className="btn upvote">
+						<ArrowUpIcon /> Upvotes<span>{post.ups}</span>
+					</button>
+					<button className="btn downvote">
+						<ArrowDownIcon /> DownVotes
+						<span>{post.downs}</span>
+					</button>
+
+					<button
+						onClick={() => {
+							// setPermalinkForComments(post.permalink)
+							console.log(post.permalink);
+						}}
+						className="btn comment right"
+					>
+						<CommentIcon /> Comments
+						<span>
+							{/* returns comments for a single post (don't worry about it man, I'll handle everything)*/}
+							{comments && comments.comments.length}
+						</span>
+					</button>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default Post;
