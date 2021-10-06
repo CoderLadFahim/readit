c3951a2e3dc7057a4708a29099e405d44ce39d84
Fahim <fahimalemroz@gmail.com>
2021-10-06 09:39:05 +0600

    Added placeholder content for deployment

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import SubredditsDisplay from './views/SubredditsDisplay';
 import Home from './views/Home';
 import CatchAll from './views/CatchAll';
 import SideButtons from './components/SideButtons';
+import Logo from './components/Logo';
 import scrollDisabler from './scrollDisabler';
 
 function App() {
@@ -17,31 +18,31 @@ function App() {
 		scrollDisabler();
 	};
 
-	useEffect(() => {
-		alert(
-			'This site is still under development, please excuse a few bugs here and there'
-		);
-	}, []);
+	// return (
+	// 	<Router>
+	// 		{showInfoModal && <InfoModal infoModalToggler={toggleInfoModal} />}
+	// 		<NavBar infoModalToggler={toggleInfoModal} />
+	// 		<SideButtons />
+	// 		<Switch>
+	// 			<Route exact path="/home">
+	// 				<Home />
+	// 			</Route>
 
+	// 			<Route exact path="/subreddits">
+	// 				<SubredditsDisplay />
+	// 			</Route>
+
+	// 			<Route exact path="*">
+	// 				<CatchAll />
+	// 			</Route>
+	// 		</Switch>
+	// 	</Router>
+	// );
 	return (
-		<Router>
-			{showInfoModal && <InfoModal infoModalToggler={toggleInfoModal} />}
-			<NavBar infoModalToggler={toggleInfoModal} />
-			<SideButtons />
-			<Switch>
-				<Route exact path="/home">
-					<Home />
-				</Route>
-
-				<Route exact path="/subreddits">
-					<SubredditsDisplay />
-				</Route>
-
-				<Route exact path="*">
-					<CatchAll />
-				</Route>
-			</Switch>
-		</Router>
+		<div classNamee="w-screen h-screen">
+			<Logo />
+			<h1 classNamee="text-blue-200">Coming Soon</h1>
+		</div>
 	);
 }
 
