3f33e7f919c87e3d91932f5b7e0179b1559d823d
Fahim <fahimalemroz@gmail.com>
2021-10-23 14:10:58 +0600

    Logo click redirects to home and shows posts from r/all without refreshing the page

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import { clearAll } from '../features/subreddits/subredditSlice';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchPosts } from '../features/posts/postsSlice';
 import { usePosts, useQuery, useSubredditData } from '../hooks';
 import PostsDisplay from '../components/PostsDisplay';
 import LeaderBoard from '../components/LeaderBoard';
@@ -10,22 +11,34 @@ function Home() {
 	// getting the subreddit to look for from query params, (coming initially from CatchAll and also from subreddit results on SubredditDisplay)
 	const query = useQuery();
 	const subredditQuery = query.get('subreddit');
+	const dispatch = useDispatch();
 
 	// returns an obj {name, iconImg, desc, subscribers}
 	const subData = useSubredditData(subredditQuery || 'all');
 
 	// the following variable and side effect, clears the subreddits when user visits the home route (DO NOT TAMPER WITH IT)
-	const subredditSliceActionDispatcher = useDispatch();
+	// const subredditSliceActionDispatcher = useDispatch();
+
+	// useEffect(() => {
+	// 	subredditSliceActionDispatcher(clearAll());
+	// }, []);
+
 	useEffect(() => {
-		subredditSliceActionDispatcher(clearAll());
-	}, []);
+		dispatch(fetchPosts(subredditQuery || 'all'));
+	}, [subredditQuery]);
 
 	/*
 	 returns an array of postObjs
 	 properties needed from the post objects: 
 	 		title, over_18 (hide the post if true), author, permalink(need this to fetch comments), subreddit_name_prefixed, ups, downs,
 	*/
-	const posts = usePosts(subredditQuery || 'all');
+
+	const posts = useSelector((state) => {
+		if (state.posts.posts) return state.posts.posts;
+		return null;
+	});
+
+	// const posts = usePosts(subredditQuery || 'all');
 	// uncomment the following useEffect blocc to study the data
 	// useEffect(() => {
 	// 	posts && console.log(posts, subData);
