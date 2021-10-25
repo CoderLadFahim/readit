e1ad2a3a2b8733fd032cf6f2b6e5eb4205cceadf
Fahim <fahimalemroz@gmail.com>
2021-10-25 18:35:10 +0600

    Adding the colours directly as hex values for CustomSubredditIcon instead of tailwind classes

--- src/components/CustomSubredditIcon.js
+++ src/components/CustomSubredditIcon.js
@@ -1,19 +1,23 @@
 function CustomSubredditIcon({ subName }) {
 	const availableColors = [
-		'red',
-		'yellow',
-		'green',
-		'blue',
-		'indigo',
-		'purple',
-		'pink',
+		'#EF4444',
+		'#F59E0B',
+		'#ECFDF5',
+		'#3B82F6',
+		'#6366F1',
+		'#8B5CF6',
+		'#EC4899',
 	];
 
 	return (
 		<div
-			className={`w-6 h-6 rounded-full bg-${
-				availableColors[Math.floor(Math.random() * availableColors.length)]
-			}-500 font-ubuntu text-sm text-center text-white shadow grid place-items-center`}
+			style={{
+				backgroundColor:
+					availableColors[
+						Math.floor(Math.random() * availableColors.length)
+					],
+			}}
+			className={`w-6 h-6 rounded-full font-ubuntu ubuntu-bold text-sm text-center text-white shadow grid place-items-center`}
 		>
 			<h1 className="text-center">{subName && subName[2].toUpperCase()}</h1>
 		</div>
