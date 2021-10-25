f0684b929d14a47cfc3b76aee131b017867d673f
Fahim <fahimalemroz@gmail.com>
2021-10-25 17:28:28 +0600

    r/all SideBtn showing everywhere but when the user is viewing posts from /r/all

--- src/components/SideButtons.js
+++ src/components/SideButtons.js
@@ -16,17 +16,17 @@ function SideButtons() {
 
 	return (
 		<div className="side-btns fixed right-4 bottom-4 flex flex-col justify-between z-10">
-			<button className="hidden side-btn transform w-9 h-9 bg-gray-200 rounded-full grid place-items-center transition hover:scale-110 shadow sm:hidden">
-				<span className="font-bold text-gray-600">r/?</span>
-			</button>
-
-			<Link
-				className={`transform side-btn w-9 h-9 bg-blue-400 rounded-full grid place-items-center transition hover:scale-110 shadow`}
-				to="/"
-				onClick={() => dispatch(clearPosts())}
-			>
-				<span className="font-bold text-white text-xs">r/all</span>
-			</Link>
+			{subredditQuery || history.location.pathname === '/subreddits' ? (
+				<Link
+					className={`transform side-btn w-9 h-9 bg-blue-400 rounded-full grid place-items-center transition hover:scale-110 shadow`}
+					to="/"
+					onClick={() => dispatch(clearPosts())}
+				>
+					<span className="font-bold text-white text-xs">r/all</span>
+				</Link>
+			) : (
+				''
+			)}
 
 			<Link
 				className={`side-btn top-communities-btn transform w-9 h-9 bg-gray-500
