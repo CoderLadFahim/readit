00d4157ebd88caaa17d118dafdbabff2f3d6ab70
CoderLadFahim <59782976+CoderLadFahim@users.noreply.github.com>
2022-07-10 10:29:16 +0600

    Imported useEffect

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 import NavBar from './components/NavBar';
 import InfoModal from './components/InfoModal';
